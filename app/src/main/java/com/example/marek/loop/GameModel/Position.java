14917b63e8756c624903b73e2949c28706aeb9fe
Piotr <piotr.pikul@student.uj.edu.pl>
2017-04-14 09:53:18 +0200

    Drobne uzupełnienie klasy Position (o stałe kierunków i liczenie sąsiadów).

--- app/src/main/java/com/example/marek/loop/GameModel/Position.java
+++ app/src/main/java/com/example/marek/loop/GameModel/Position.java
@@ -2,31 +2,49 @@ package com.example.marek.loop.GameModel;
 
 /**
  * Created by marek on 11.04.17.
+ * Updated by Piotr on 13.04.17
  */
 
-public class Position {
+public class Position
+{
     private int x,y;
 
+    public Position(int xx, int yy) { x=xx; y=yy; }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
     @Override
-    public boolean equals(Object o) {
+    public boolean equals(Object o)
+    {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-
         Position position = (Position) o;
-
-        if (x != position.x) return false;
-        return y == position.y;
-
+        return y == position.y && x==position.x;
     }
 
     @Override
-    public int hashCode() {
-        int result = x;
-        result = 31 * result + y;
-        return result;
+    public int hashCode()
+    {
+        return 65537 * x + y;
     }
 
-    public Position getNeighbour (short direction){
-        return null;
+    public Position getNeighbour (short direction)
+    {
+        if (direction<0 || direction >3) throw new IllegalArgumentException("Invalid direction.");
+        return new Position(
+                x+(1-(direction & 1))*(1-direction),
+                y+(direction & 1)*(direction-2) );
     }
+
+    // Stałe opisujące kierunki
+    public static final short EAST =0;
+    public static final short NORTH=1;
+    public static final short WEST =2;
+    public static final short SOUTH=3;
 }
