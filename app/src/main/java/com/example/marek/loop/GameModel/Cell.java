ec6949acfdf057ff96e8eb76060fc26d802259a1
Piotr <piotr.pikul@student.uj.edu.pl>
2017-04-14 17:25:55 +0200

    Drobne uzupełnienie klasy Position (o stałe kierunków i liczenie sąsiadów).

--- app/src/main/java/com/example/marek/loop/GameModel/Cell.java
+++ app/src/main/java/com/example/marek/loop/GameModel/Cell.java
@@ -1,34 +1,105 @@
 package com.example.marek.loop.GameModel;
 
+import java.util.Arrays;
+
 /**
  * Created by marek on 11.04.17.
+ * Beeing updated by Piotr since 13.04.17
  */
 
-public class Cell {
+public abstract class Cell
+{
     private Cell[] neighbours;
     private Position position;
 
     public Cell(Position position) {
         this.position = position;
+        neighbours = new Cell[4];
+    }
+
+    protected Cell(Cell other) //
+    {
+        this.position = other.position;
         this.neighbours = new Cell[4];
+        for(int dir =0; dir<4; dir++)
+        {
+            if (other.neighbours[dir]!=null)
+                append(dir,other.neighbours[dir]);
+        }
     }
 
-    public Cell() {
+    public final Position getPosition()
+    {
+        return position;
     }
 
-    public Cell getNeighbour(short direction) {
-        return null;
+    public Cell getNeighbour(int direction)
+    {
+        if (direction<0 || direction >3) throw new IllegalArgumentException("Invalid direction.");
+        return neighbours[direction];
     }
 
-    public short getDirection(Cell nightbour){
-        return 0;
+    public int getDirection(Cell nighbour)
+    {
+        if (nighbour==null) return -1;
+        // Rozwiązanie obliczeniowe
+        int dx = 1+position.getX()-nighbour.position.getX();
+        int dy = 1-position.getY()+nighbour.position.getY();
+        /* Jeśli mamy do czynienia z sąsiadami, (dx,dy) jest postaci:
+            (1,2) - siąsiad południowy  (2,1) - sąsiad zachodni
+            (1,0) - sąsiad północny     (0,1) - sąsiad wschodni
+        */
+        // iloczyn dx*dy jest 0 lub 2 i jedna z nich jest 1 - pełna charakteryzacja
+        int c=dx*dy;
+        if ( (c | 2)!=2 || (dx+dy-c)!=1 ) return -1; // To nie są sąsiedzi
+        int direction= ( (dx & 1)+ c ); // Wygląda niejasno, ale działa.
+        /*
+        short direction=-1; // Rozwiązanie czytelne:
+        if (nighbour.getX() == getX()-1) direction = Position.WEST;
+        if (nighbour.getX() == getX()+1) direction = Position.EAST;
+        if (nighbour.getY() == getY()-1) direction = Position.NORTH;
+        if (nighbour.getY() == getY()+1) direction = Position.SOUTH;
+        if (direction<0) return -1;
+        // */
+        if (neighbours[direction]!=nighbour) // Sąsiedzi wg. współrzędnych, ale coś się nie zgadza
+            throw new IllegalStateException("Invalid cell graph configuration!");
+        return direction;
     }
 
-    public void append (short direction, Tile tile){  // co ma ta funkcja zwracac? i czy przyjmuje Tile, czy Cell?
+    /** Ta metoda reaguje na zmianę sąsiada.
+     * Będzie jej używała klasa EmptyCell aby poznać swoje otoczenie,
+     * a klasa tile będzie
+      */
+    protected void fireAppend(int direction, Cell cell) {}
 
+    // Ta metoda
+    public void append(int direction, Cell cell)
+    {
+        if (direction<0 || direction >3) throw new IllegalArgumentException("Invalid direction.");
+        if (neighbours[direction]==cell) return;
+        if (neighbours[direction]!=null)
+            neighbours[direction].neighbours[direction ^2]=null; // Skasuj starego sąsiada
+        fireAppend(direction,cell); // Zastanów się, czy zmiana sasiada coś znaczy
+        cell.fireAppend(direction ^2,this); // Powiadom sąsiada o zmianie
+        neighbours[direction] =cell; // Ustaw nowego sąsiada
+        cell.neighbours[direction ^ 2]=this; // Ustaw sąsiada (this) nowemu sąsiadowi
     }
 
-    public boolean isTile(){  // czy to jest potrzebne?
+    public boolean isTile()
+    {
         return false;
     }
+
+    /**
+     * Usuwa wszystkie grafowe połączenia komórki. Wywoływać dla pewności, przy usuwaniu z grafu.
+     */
+    protected void forgetMe()
+    {
+        for (int dir = 0; dir < 3; dir++)
+            if (neighbours[dir] != null)
+                neighbours[dir].neighbours[dir ^ 2] = null;
+    }
+
+    // Maski bitowe kierunków na potrzeby klas konretnych
+    protected static final byte[] mask= new byte[]{1,2,4,8};
 }
--- app/src/main/java/com/example/marek/loop/GameModel/Position.java
+++ app/src/main/java/com/example/marek/loop/GameModel/Position.java
@@ -2,7 +2,7 @@ package com.example.marek.loop.GameModel;
 
 /**
  * Created by marek on 11.04.17.
- * Updated by Piotr on 13.04.17
+ * Beeing updated by Piotr since 13.04.17.
  */
 
 public class Position
@@ -11,13 +11,20 @@ public class Position
 
     public Position(int xx, int yy) { x=xx; y=yy; }
 
-    public int getX() {
-        return x;
+    /** Metoda dekodująca - tworzy Position na podstawie hashCode
+     * Powinna działać, ale trzeba przetestować
+     */
+    public static Position fromHashCode(int hash)
+    {
+        hash-= 65537/2;
+        int x= hash >0 ? hash/65537 : hash/65537 -1;
+        int y= hash- x*65537+ 65537/2; // poprawiona reszta z dzielenia
+        return new Position(x,y);
     }
 
-    public int getY() {
-        return y;
-    }
+    // Dostęp do współrzędnych
+    public int getX() { return x; }
+    public int getY() { return y; }
 
     @Override
     public boolean equals(Object o)
@@ -30,21 +37,40 @@ public class Position
 
     @Override
     public int hashCode()
-    {
-        return 65537 * x + y;
+    { // Duża liczba pierwsza zapewnia większa niepowtarzalność, skoro x i y są "bliskie" 0.
+        return 65537 * x + y; // W praktyce będzie to kodowanie 1 do 1 :)
     }
 
-    public Position getNeighbour (short direction)
+    public Position getNeighbour (int direction)
     {
         if (direction<0 || direction >3) throw new IllegalArgumentException("Invalid direction.");
         return new Position(
-                x+(1-(direction & 1))*(1-direction),
-                y+(direction & 1)*(direction-2) );
+                x+ (1-(direction & 1))*(1-direction),
+                y+ (direction & 1)*(direction-2) );
+    }
+
+    @Override
+    public String toString() // To może się przydać do debugowania
+    {
+        return String.format("(%3d,%3d)",x,y);
+    }
+
+    // Oblicza różnicę współrzędnych w zadanym kierunku.
+    public int distanceInDirection(Position other, int direction)
+    {
+        switch (direction)
+        {
+            case NORTH: return other.y - y; // other jest (powinien być) powyżej
+            case SOUTH: return y - other.y; // other jest (powinien być) poniżej
+            case EAST : return other.x - x; // other jest (powinien być) po prawej
+            case WEST : return x - other.x; // other jest (powinien być) po lewej
+        }
+        throw new IllegalArgumentException("Invalid direction.");
     }
 
     // Stałe opisujące kierunki
-    public static final short EAST =0;
-    public static final short NORTH=1;
-    public static final short WEST =2;
-    public static final short SOUTH=3;
+    public static final int EAST =0;
+    public static final int NORTH=1;
+    public static final int WEST =2;
+    public static final int SOUTH=3;
 }
